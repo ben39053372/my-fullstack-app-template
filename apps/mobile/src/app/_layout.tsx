54d1e806384611055e486af32db0251a6bd42e70
Ben Cheng <ben39053372@gmail.com>
2026-05-18 17:12:57 +0800

    feat(mobile): add mobile application directory

---
+++ apps/mobile/src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+
+export default function Layout() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hide();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
